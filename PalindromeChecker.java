e55ac7123807d405eeb4e08d78c4bbe52eb64821
nananananani <ayuuki478@gmail.com>
2026-03-08 15:46:05 +0530

    UC2: Hardcoded palindrome

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
+       // UC1
+
         System.out.println("==== Palindrome Checker App ====");
         System.out.println("Welcome User");
         System.out.print("Enter a string:");
         String input = sc.nextLine();
         System.out.println("You entered: " + input);
 
+        // UC2
+        String word = "madam";
+        String rev = "";
+        for(int i = word.length()-1; i>=0; i--){
+        rev += word.charAt(i);
+    }
+        if(word.equals(rev)){
+        System.out.println("Palindrome");}
+        else{
+        System.out.println("Not Palindrome");
+        }   
     }
 }
