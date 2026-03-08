8c8cfeede99a76a006d27d761a43d37d4c8ed32f
nananananani <ayuuki478@gmail.com>
2026-03-08 15:33:11 +0530

    Base application setup

---
+++ PalindromeChecker.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==== Palindrome Checker App ====");
+        System.out.print("Enter a string:");
+        String input = sc.nextLine();
+        System.out.println("You entered: " + input);
+
+    }
+}
